3c39a9ea58a25cea00286dd38cbbc4ee61428290
Lucas Holanda <luks206@gmail.com>
2022-07-30 22:32:39 -0300

    Add Shared Project

--- Mini.Shared/Minificador.cs
+++ Mini.Shared/Minificador.cs
@@ -2,6 +2,11 @@
 {
     public static class Minificador
     {
+        /*public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+        }*/
+
         public static void Executar(string[] args)
         {
             var origem = string.Empty; //Recebe o nome do arquivo de origem
@@ -22,6 +27,7 @@
             var dadosOrigem = File.ReadAllText(origem); //lê os arquivos de origem
             var dadosDestino = dadosOrigem.Replace(Environment.NewLine, ""); //remove as quebras de linha
             dadosDestino = string.Join(" ", dadosDestino.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)); //retira espaços desnecessários
+            
             File.WriteAllText(destino, dadosDestino); //escreve a versão minimalizada no arquivo de destino
         }
 
