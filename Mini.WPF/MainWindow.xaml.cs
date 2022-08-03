d2fbbbeece9a7d0283f98ae6a50309128fb1b516
Lucas Holanda <luks206@gmail.com>
2022-08-02 21:31:45 -0300

    Criado WPF

---
+++ Mini.WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Mini.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mini.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExec_Click(object sender, RoutedEventArgs e)
+        {
+            Minificador.Executar(new string[] { "/o=" + Origem.Text, "/d=" + Destino.Text });
+        }
+    }
+}
